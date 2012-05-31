abec25de98cf5bfe398a6ed148e32242b02762f7
Chris Cornutt <enygma@phpdeveloper.org>
2012-05-31 15:29:39 -0500

    adding the DB config with the connection info

---
+++ app/config/db.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Base Database Config.
+ *
+ * See the individual environment DB configs for specific config information.
+ */
+
+return array(
+	'active' => 'default',
+
+	/**
+	 * Base config, just need to set the DSN, username and password in env. config.
+	 */
+	'default' => array(
+		'type'        => 'mysql',
+		'connection'  => array(
+			'persistent' => false,
+			'hostname' => 'localhost',
+			'database' => 'jobtrack',
+			'username' => 'jobtrack',
+			'password' => 'jt42'
+		),
+		'identifier'   => '`',
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => false,
+		'profiling'    => false,
+	),
+
+	'redis' => array(
+		'default' => array(
+			'hostname'  => '127.0.0.1',
+			'port'      => 6379,
+		)
+	),
+
+);
