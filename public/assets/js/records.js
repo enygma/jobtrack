a4d7fb210c6c02194f1ac09111a0fb54138d70f0
Chris Cornutt <enygma@phpdeveloper.org>
2012-06-02 13:49:49 -0500

    splitting up the search and records/form handling out of one app.js file

---
+++ public/assets/js/records.js
@@ -0,0 +1,111 @@
+$(function() {
+	var RecordModel = Backbone.Model.extend({
+		defaults: {
+			full_name: 'Test Name',
+			id: 0
+		}
+	});
+
+	//var record = new RecordModel({});
+
+	var RecordCollection = Backbone.Collection.extend({
+		url: '/record/index'
+	});
+	var records = new RecordCollection({model:RecordModel});
+
+	var RecordFormView = Backbone.View.extend({
+		events: {
+			'click #submit-record' : 'submitRecordForm',
+		},
+
+		initialize: function() {
+			// nothing to see, move along
+		},
+
+		submitRecordForm: function(evt) {
+			evt.preventDefault(); // prevent the form submit
+
+			var recordId = $('input[name=record_id]').val();
+			if (recordId > 0) {
+				// PUT request - find the record
+				var record = this.collection.where({id:recordId});
+
+				record[0].set({
+					full_name: $('input[name=full_name]').val()
+				});
+
+				record[0].save();
+
+			} else {
+				// POST request
+				records.create({
+					full_name: $('input[name=full_name]').val()
+				});
+			}
+
+			$('#record-form :input').val('');
+
+			// refresh the list view too
+			this.options.list.refreshList();
+		}
+	});
+
+	var RecordListView = Backbone.View.extend({
+		template: _.template('<a class="record-link" href="<%= id %>"><%= full_name %></a><br/>'),
+
+		events: {
+			'click .record-link' : 'loadRecord'
+		},
+
+		initialize: function() {
+			this.collection.bind('reset',this.render,this);
+		},
+		render: function() {
+			var self = this;
+			this.collection.each(function(record){
+				self.$el.append(self.template( record.toJSON() ));
+			});
+			return this;
+		},
+		loadRecord: function(evt) {
+			evt.preventDefault();
+
+			// find the record and populate the form with it
+			var recordId = $(evt.currentTarget).attr('href');
+			var record 	 = this.collection.where({id:recordId});
+
+			// set the form values
+			$('input[name=record_id]').val(record[0].get('id'));
+			$('input[name=full_name]').val(record[0].get('full_name'));
+		},
+		refreshList: function() {
+			this.$el.html('');
+			this.collection.fetch();
+		},
+		searchRecords: function() {
+			console.log('search!');
+
+			var searchQuery = $('#form_query').val();
+			console.log(searchQuery);
+			console.log(this.collection.url);
+
+			return this;
+		}
+	});
+
+	// display the latest records in the sidebar
+	var listView = new RecordListView({
+		el: $('#latest-records'),
+		collection: records,
+		searchBtn: $('#search-btn')
+	});
+
+	// set up form handling
+	var formView = new RecordFormView({
+		el: $('#record-form'),
+		collection: records,
+		list: listView
+	});
+
+	records.fetch();
+});
---
+++ public/assets/js/search.js
@@ -0,0 +1,97 @@
+$(function(){
+
+	var SearchModel = Backbone.Model.extend({
+		defaults: {
+			full_name: 'Test Name',
+			id: 0
+		}
+	});
+
+	var SearchCollection = Backbone.Collection.extend({
+		url: '/record/search'
+	});
+
+	var search = new SearchCollection({model:SearchModel});
+
+	var SearchResultsView = Backbone.View.extend({
+		template: _.template('<a class="record-link" href="<%= id %>"><%= full_name %></a><br/>'),
+		events: {
+			'click .clear-results' : 'clearResults',
+			'click .record-link'   : 'loadRecord'
+		},
+
+		render: function() {
+			var self = this;
+
+			// fetch the search results and fill in the suggestions box
+			this.$el.css({'display':'block'});
+			this.$el.html('<h3>Search Results</h3>');
+
+			if (this.collection.length > 0) {
+				this.collection.each(function(record){
+					self.$el.append(self.template( record.toJSON() ));
+				});
+			} else {
+				this.$el.append('<b>No results found</b><br/>');
+			}
+
+			this.$el.append('<a class="clear-results" href="#">Clear Results</a><br/>');
+			return this;
+		},
+		clearResults: function() {
+			$('#form_query').val('');
+			this.$el.css('display','none');
+			this.$el.html('');
+		},
+
+		loadRecord: function(evt) {
+			evt.preventDefault();
+
+			// find the record and populate the form with it
+			var recordId = $(evt.currentTarget).attr('href');
+			var record 	 = this.collection.where({id:recordId});
+
+			// set the form values
+			$('input[name=record_id]').val(record[0].get('id'));
+			$('input[name=full_name]').val(record[0].get('full_name'));
+		},
+	})
+
+	var SearchView = Backbone.View.extend({
+		
+		events: {
+			'click #search-btn' : 'searchRecords',
+		},
+
+		initialize: function() {
+			console.log('init search view');
+		},
+		searchRecords: function(event) {
+		
+			var self = this;	
+			var searchQuery = $('#form_query').val();
+
+			this.collection.fetch({
+				data:{query:searchQuery},
+				success: function() {
+					self.options.resultsView.render();
+				}
+			});
+		},
+		clearResults: function() {
+			console.log('clear');
+		}
+	});
+
+	var rview = new SearchResultsView({
+		el: $('#search-results'),
+		collection: search
+	});
+	var sview = new SearchView({
+		el: $('.form-search'),
+		collection: search,
+		resultsView: rview
+	});
+
+// end onReady
+});
